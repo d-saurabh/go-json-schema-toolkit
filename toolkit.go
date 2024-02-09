9e5ee1c1b752a5b923f17a5a44621a62be1e8fa2
d-saurabh <saurabhdeshpande49@gmail.com>
2024-02-09 07:54:05 +0100

    new changes

--- toolkit.go
+++ toolkit.go
@@ -2,6 +2,7 @@ package gojsonschematoolkit
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/xeipuuv/gojsonschema"
@@ -33,40 +34,135 @@ func NewSchemaManager(schemaMap map[string]interface{}) SchemaManager {
 	}
 }
 
+// const BaseSchema = `
+// {
+//     "$schema": "http://json-schema.org/draft-07/schema#",
+//     "type": "object",
+//     "properties": {
+//         "id": {
+//             "type": "string"
+//         }
+//     },
+//     "required": ["id"],
+//     "additionalProperties": false
+// }
+// `
 const BaseSchema = `
 {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "type": "object",
-    "properties": {
-        "id": {
-            "type": "string"
-        }
-    },
-    "required": ["id"],
-    "additionalProperties": false
-}
+	"definitions": {
+	  "group": {
+		"type": "object",
+		"properties": {
+		  "id": {
+			"type": "string"
+		  },
+		  "name": {
+			"type": "string"
+		  }
+		},
+		"required": ["id", "name"]
+	  },
+	  "social": {
+		"type": "object",
+		"properties": {
+		  "network": {
+			"type": "string"
+		  },
+		  "username": {
+			"type": "string"
+		  }
+		},
+		"required": ["network", "username"]
+	  },
+	  "attribute": {
+		"type": "object",
+		"additionalProperties": true
+	  }
+	},
+	"type": "object",
+	"properties": {
+	  "user": {
+		"type": "object",
+		"properties": {
+		  "id": {
+			"type": "string"
+		  },
+		  "name": {
+			"type": "string"
+		  },
+		  "groups": {
+			"type": "array",
+			"items": {
+			  "$ref": "#/definitions/group"
+			}
+		  },
+		  "socials": {
+			"type": "array",
+			"items": {
+			  "$ref": "#/definitions/social"
+			}
+		  },
+		  "attributes": {
+			"$ref": "#/definitions/attribute"
+		  }
+		},
+		"additionalProperties": false
+	  }
+	}
+  }
 `
 
 // CreateSchema creates a new schema with the given ID and additional properties.
 // The additional properties are merged with the base schema to form the new schema.
 // It returns an error if the schema cannot be created.
 func (sm *SchemaManagerImpl) CreateSchema(schemaID string, additionalSchema string) error {
-	var base map[string]interface{}
-	var props map[string]interface{}
-
 	// Unmarshal the base schema and the additional schema
+	var base, additional map[string]interface{}
 	if err := json.Unmarshal([]byte(BaseSchema), &base); err != nil {
 		return err
 	}
-	if err := json.Unmarshal([]byte(additionalSchema), &props); err != nil {
+	if err := json.Unmarshal([]byte(additionalSchema), &additional); err != nil {
 		return err
 	}
 
-	// Merge the base schema with the additional properties
+	// Retrieve the "properties" field from the base schema and the additional schema
 	baseProperties := base["properties"].(map[string]interface{})
-	additionalProperties := props["properties"].(map[string]interface{})
+	additionalProperties := additional["properties"].(map[string]interface{})
+
+	// Check if the additional schema's properties contain a "user" property and no other properties
+	if _, ok := additionalProperties["user"]; !ok {
+		return errors.New("additional schema 'properties' object does not contain a 'user' property")
+	}
+	if len(additionalProperties) > 1 {
+		return errors.New("additional schema 'properties' object contains properties other than 'user'")
+	}
+
+	// Determine which entity to extend
 	for k, v := range additionalProperties {
-		baseProperties[k] = v
+		if _, ok := baseProperties[k]; ok {
+			// If the key exists in the base schema, extend that entity
+			targetProperties := baseProperties[k].(map[string]interface{})["properties"].(map[string]interface{})
+			for kk, vv := range v.(map[string]interface{})["properties"].(map[string]interface{}) {
+				targetProperties[kk] = vv
+			}
+		}
+	}
+
+	// Merge the definitions
+	baseDefinitions := base["definitions"].(map[string]interface{})
+	additionalDefinitions, ok := additional["definitions"].(map[string]interface{})
+	if ok {
+		for k, v := range additionalDefinitions {
+			baseDefinitions[k] = v
+		}
+	}
+
+	// Convert the final merged schema to a JSON string
+	mergedSchemaJson, err := json.MarshalIndent(base, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting merged schema to JSON:", err)
+	} else {
+		fmt.Println("Final merged schema:", string(mergedSchemaJson))
 	}
 
 	// Add the new schema to the schema map
--- toolkit_test.go
+++ toolkit_test.go
@@ -65,3 +65,291 @@ func TestSchemaManager(t *testing.T) {
 		})
 	}
 }
+
+func TestSchemaManager_1(t *testing.T) {
+	sm := gojsonschematoolkit.NewSchemaManager(make(map[string]interface{}))
+
+	// Test CreateSchema
+	err := sm.CreateSchema("userSchema", `{
+        "properties": {
+          "user": {
+            "properties": {
+              "email": {
+                "type": "string",
+                "format": "email"
+              }
+            },
+			"additionalProperties": false
+          }
+        },
+        "definitions": {
+          "attribute": {
+            "properties": {
+              "DOB": {
+                "type": "string",
+                "format": "date"
+              }
+            },
+			"additionalProperties": false,
+            "type": "object"
+          },
+          "newDefinition": {
+            "properties": {
+              "newProperty": {
+                "type": "string"
+              }
+            },
+            "type": "object"
+          }
+        }
+      }`)
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name        string
+		schemaID    string
+		data        string
+		expectError bool
+	}{
+		{
+			name:     "Valid user data",
+			schemaID: "userSchema",
+			data: `{
+                "user": {
+                    "id": "1",
+                    "name": "John Doe",
+                    "email": "john.doe@example.com",
+                    "groups": [
+                        {
+                            "id": "group1",
+                            "name": "Group 1"
+                        }
+                    ],
+                    "socials": [
+                        {
+                            "network": "Twitter",
+                            "username": "johndoe"
+                        }
+                    ],
+                    "attributes": {
+                        "DOB": "1990-01-01"
+                    }
+                }
+            }`,
+			expectError: false,
+		},
+		{
+			name:     "Invalid user data - missing email",
+			schemaID: "userSchema",
+			data: `{
+		        "user": {
+		            "id": "1",
+		            "name": "John Doe",
+		            "DOB": "1990-01-01",
+		            "groups": [
+		                {
+		                    "id": "group1",
+		                    "name": "Group 1"
+		                }
+		            ],
+		            "socials": [
+		                {
+		                    "network": "Twitter",
+		                    "username": "johndoe"
+		                }
+		            ],
+		            "attributes": {
+		                "DOB": "1990-01-01",
+		                "newProperty": "newValue"
+		            }
+		        }
+		    }`,
+			expectError: true,
+		},
+		// Add more test cases here
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := sm.ValidateSchema(tc.schemaID, tc.data)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestSchemaManager_2(t *testing.T) {
+	sm := gojsonschematoolkit.NewSchemaManager(make(map[string]interface{}))
+
+	// Test CreateSchema with userSchema
+	err := sm.CreateSchema("userSchema", `{
+        "properties": {
+          "user": {
+            "properties": {
+              "email": {
+                "type": "string",
+                "format": "email"
+              }
+            },
+            "additionalProperties": false
+          }
+        },
+        "definitions": {
+          "attribute": {
+            "properties": {
+              "DOB": {
+                "type": "string",
+                "format": "date"
+              }
+            },
+            "additionalProperties": false,
+            "type": "object"
+          },
+          "newDefinition": {
+            "properties": {
+              "newProperty": {
+                "type": "string"
+              }
+            },
+            "type": "object"
+          }
+        }
+      }`)
+	assert.NoError(t, err)
+
+	// Test CreateSchema with invalid schema (properties other than 'user')
+	err = sm.CreateSchema("invalidSchema", `{
+        "properties": {
+          "item": {
+            "properties": {
+              "name": {
+                "type": "string"
+              },
+              "price": {
+                "type": "number"
+              }
+            },
+            "additionalProperties": false
+          }
+        }
+      }`)
+	assert.Error(t, err)
+
+	// Test CreateSchema with valid schema (only 'user' property)
+	err = sm.CreateSchema("validSchema", `{
+        "properties": {
+          "user": {
+            "properties": {
+              "name": {
+                "type": "string"
+              },
+              "price": {
+                "type": "number"
+              }
+            },
+            "additionalProperties": false
+          }
+        }
+      }`)
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name        string
+		schemaID    string
+		data        string
+		expectError bool
+	}{
+		// {
+		// 	name:     "Valid user data",
+		// 	schemaID: "userSchema",
+		// 	data: `{
+		//         "user": {
+		//             "id": "1",
+		//             "name": "John Doe",
+		//             "email": "john.doe@example.com",
+		//             "groups": [
+		//                 {
+		//                     "id": "group1",
+		//                     "name": "Group 1"
+		//                 }
+		//             ],
+		//             "socials": [
+		//                 {
+		//                     "network": "Twitter",
+		//                     "username": "johndoe"
+		//                 }
+		//             ],
+		//             "attributes": {
+		//                 "DOB": "1990-01-01"
+		//             }
+		//         }
+		//     }`,
+		// 	expectError: false,
+		// },
+		// {
+		// 	name:     "Invalid user data - missing email",
+		// 	schemaID: "userSchema",
+		// 	data: `{
+		//         "user": {
+		//             "id": "1",
+		//             "name": "John Doe",
+		//             "DOB": "1990-01-01",
+		//             "groups": [
+		//                 {
+		//                     "id": "group1",
+		//                     "name": "Group 1"
+		//                 }
+		//             ],
+		//             "socials": [
+		//                 {
+		//                     "network": "Twitter",
+		//                     "username": "johndoe"
+		//                 }
+		//             ],
+		//             "attributes": {
+		//                 "DOB": "1990-01-01",
+		//                 "newProperty": "newValue"
+		//             }
+		//         }
+		//     }`,
+		// 	expectError: true,
+		// },
+		// Test case for valid data with 'validSchema'
+		// {
+		// 	name:     "Valid user data with validSchema",
+		// 	schemaID: "validSchema",
+		// 	data: `{
+		//         "user": {
+		//             "name": "John Doe",
+		//             "price": 100
+		//         }
+		//     }`,
+		// 	expectError: false,
+		// },
+		// Test case for invalid data with 'validSchema' (missing 'name')
+		{
+			name:     "Invalid user data with validSchema - missing name",
+			schemaID: "validSchema",
+			data: `{
+		        "user": {
+		            "price": 100
+		        }
+		    }`,
+			expectError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := sm.ValidateSchema(tc.schemaID, tc.data)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
